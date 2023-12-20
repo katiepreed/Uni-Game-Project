cf263fa3f55673438aa35092ebeac24f0e6747d3
Katie Reed <Katiepreed@yahoo.co.uk>
2023-12-20 19:36:01 +0000

    Trees and Mountain and Clouds and Sun

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var isRight;
 var isFalling;
 var isPlummeting;
 var speed;
+var cameraPosX;
+var inCanyon;
 
 // Items
 var collectable;
@@ -28,6 +30,7 @@ function setup() {
   isFalling = false;
   isPlummeting = false;
   speed = 3;
+  cameraPosX = 0;
 
   collectable = {
     x_pos: 420,
@@ -36,47 +39,57 @@ function setup() {
     isFound: false,
   };
 
-  canyon = {
-    x_pos: 300,
+  canyons = {
+    x_pos: [300, 800, 1200],
     y_pos: floorPos_y,
     width: 100,
   };
 
-  tree = {
-    x_pos: [50, 150, 550, 450],
+  trees = {
+    x_pos: [50, 250, 450, 750, 1120, 1400, 1750, 1900],
     y_pos: floorPos_y,
   };
 
-  cloud = {
-    x_pos: [200, 400, 700],
-    y_pos: 150,
-    size: 70,
+  clouds = {
+    x_pos: [200, 300, 600, 800, 100, 1200, 1400, 1600, 1800, 2000],
+    y_pos: [50, 200, 150, 80, 150, 20, 150, 180, 100, 200],
+    size: 50,
+    scale: [1, 1.5, 2, 1, 1, 1.5, 2, 1, 2, 1],
   };
 
-  mountain = {
-    x_pos: [170, 680],
+  mountains = {
+    x_pos: [5, 450, 600, 920, 910, 1400, 1700],
     y_pos: floorPos_y,
+    size: 200,
+    scale: [1.4, 1.3, 0.7, 1.4, 0.6, 1.5, 1],
   };
 }
 
 function draw() {
-  background(100, 155, 255);
+  background(208, 255, 150);
 
   noStroke();
-  fill(0, 155, 0);
+  fill(38, 153, 145);
   rect(0, floorPos_y, width, height - floorPos_y);
 
-  canyon_draw();
+  drawSun();
 
-  cloud.x_pos.forEach((x_pos) => {
-    draw_cloud(x_pos);
+  push();
+  translate(-cameraPosX, 0);
+
+  canyons.x_pos.forEach((x_pos) => {
+    draw_canyon(x_pos, canyons.y_pos, canyons.width);
   });
 
-  mountain.x_pos.forEach((x_pos) => {
-    draw_mountain(x_pos);
+  clouds.x_pos.forEach((x_pos, i) => {
+    draw_cloud(x_pos, clouds.y_pos[i], clouds.size, clouds.scale[i]);
   });
 
-  tree.x_pos.forEach((x_pos) => {
+  mountains.x_pos.forEach((x_pos, i) => {
+    draw_mountain(x_pos, mountains.y_pos, mountains.size, mountains.scale[i]);
+  });
+
+  trees.x_pos.forEach((x_pos) => {
     draw_tree(x_pos);
   });
 
@@ -90,7 +103,7 @@ function draw() {
     collectable_draw();
   }
 
-  fill(255, 255, 255);
+  fill(130, 213, 255);
 
   if (isLeft && isFalling) {
     charLeftFalling();
@@ -106,7 +119,20 @@ function draw() {
     charFront();
   }
 
-  var inCanyon = gameChar_x >= 310 && gameChar_x <= 390;
+  pop();
+
+  for (i = 0; i <= canyons.x_pos.length; i++) {
+    if (
+      gameChar_x > canyons.x_pos[i] + 10 &&
+      gameChar_x < canyons.x_pos[i] + canyons.width - 10
+    ) {
+      inCanyon = true;
+      break;
+    } else {
+      inCanyon = false;
+    }
+  }
+
   var aboveGround = gameChar_y < floorPos_y;
 
   if (inCanyon && !aboveGround) {
@@ -116,12 +142,14 @@ function draw() {
   if (isPlummeting) {
     gameChar_y += speed * 2;
   } else {
-    if (isLeft) {
+    if (isLeft && gameChar_x > 0) {
       gameChar_x -= speed;
+      cameraPosX = gameChar_x > 500 ? cameraPosX - speed : cameraPosX;
     }
 
     if (isRight) {
       gameChar_x += speed;
+      cameraPosX = gameChar_x > 500 ? cameraPosX + speed : cameraPosX;
     }
 
     if (isFalling) {
@@ -151,7 +179,7 @@ function keyPressed() {
     isFalling = true;
 
     if (gameChar_y == floorPos_y) {
-      gameChar_y -= 100;
+      gameChar_y -= 150;
     }
   }
 }
@@ -176,107 +204,133 @@ function collectable_draw() {
   circle(collectable.x_pos, collectable.y_pos, collectable.size - 8);
 }
 
-function canyon_draw() {
+function drawSun() {
+  fill(255, 202, 128);
+  circle(10, 10, 200);
+  fill(242, 255, 128);
+  circle(10, 10, 180);
+}
+
+function draw_canyon(x_pos, y_pos, width) {
   noStroke();
-  fill(100, 155, 255);
-  rect(canyon.x_pos, canyon.y_pos, canyon.width, canyon.width * 2);
+  fill(208, 255, 150);
+  rect(x_pos, y_pos, width, width * 2);
 }
 
-function draw_tree(treePosX) {
-  fill(156, 114, 62);
+function draw_tree(x_pos) {
+  fill(117, 41, 89);
   quad(
-    treePosX - 10,
-    tree.y_pos - 150,
-    treePosX + 5,
-    tree.y_pos - 150,
-    treePosX + 15,
-    tree.y_pos,
-    treePosX - 15,
-    tree.y_pos
+    x_pos - 3,
+    trees.y_pos - 80,
+    x_pos + 3,
+    trees.y_pos - 80,
+    x_pos + 8,
+    trees.y_pos,
+    x_pos - 8,
+    trees.y_pos
   );
 
-  fill(18, 143, 7);
-  circle(treePosX - 20, tree.y_pos - 150, 44);
-  circle(treePosX + 5, tree.y_pos - 130, 44);
-  circle(treePosX + 25, tree.y_pos - 150, 44);
-  circle(treePosX + 5, tree.y_pos - 170, 44);
+  fill(135, 5, 64);
+  circle(x_pos - 20, trees.y_pos - 130, 44);
+  circle(x_pos + 5, trees.y_pos - 110, 44);
+  circle(x_pos + 25, trees.y_pos - 120, 44);
+  circle(x_pos + 5, trees.y_pos - 150, 44);
 
-  fill(14, 196, 96);
-  circle(treePosX + 5, tree.y_pos - 125, 37);
-  circle(treePosX + 25, tree.y_pos - 110, 37);
-  circle(treePosX + 40, tree.y_pos - 120, 37);
-  circle(treePosX + 25, tree.y_pos - 140, 37);
+  fill(219, 83, 97);
+  circle(x_pos + 5, trees.y_pos - 105, 37);
+  circle(x_pos + 25, trees.y_pos - 90, 37);
+  circle(x_pos + 40, trees.y_pos - 100, 37);
+  circle(x_pos + 25, trees.y_pos - 120, 37);
 
-  fill(65, 181, 88);
-  circle(treePosX - 5, tree.y_pos - 115, 35);
-  circle(treePosX - 25, tree.y_pos - 95, 35);
-  circle(treePosX - 40, tree.y_pos - 105, 35);
-  circle(treePosX - 25, tree.y_pos - 125, 35);
+  fill(230, 123, 53);
+  circle(x_pos - 5, trees.y_pos - 95, 35);
+  circle(x_pos - 25, trees.y_pos - 75, 35);
+  circle(x_pos - 40, trees.y_pos - 85, 35);
+  circle(x_pos - 25, trees.y_pos - 105, 35);
 }
 
-function draw_cloud(x_pos) {
+function draw_cloud(x_pos, y_pos, size, scale) {
   fill(255, 255, 255);
-  circle(x_pos, cloud.y_pos, cloud.size);
-  circle(x_pos - 30, cloud.y_pos, cloud.size - 20);
-  circle(x_pos + 30, cloud.y_pos, cloud.size - 20);
+  new_size = size * scale;
+
+  circle(x_pos, y_pos, new_size);
+  circle(
+    x_pos - (8 * new_size) / 16,
+    y_pos - (2 * new_size) / 16,
+    (8 * new_size) / 16
+  );
+  circle(
+    x_pos + (7 * new_size) / 16,
+    y_pos - (2 * new_size) / 16,
+    (6 * new_size) / 16
+  );
+
+  ellipse(
+    x_pos - (12 * new_size) / 16,
+    y_pos + (3 * new_size) / 16,
+    (12 * new_size) / 16,
+    (8 * new_size) / 16
+  );
+
+  ellipse(
+    x_pos + (9 * new_size) / 16,
+    y_pos + (2 * new_size) / 16,
+    (12 * new_size) / 16,
+    (8 * new_size) / 16
+  );
+
+  ellipse(
+    x_pos,
+    y_pos + (5 * new_size) / 16,
+    (16 * new_size) / 16,
+    (7 * new_size) / 16
+  );
 }
 
-function draw_mountain(x_pos) {
-  fill(126, 158, 171);
+function draw_mountain(x_pos, y_pos, size, scale) {
+  new_size = size * scale;
+
+  // base
+  fill(201, 181, 232);
   triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos - 100,
-    mountain.y_pos,
+    x_pos + new_size / 2,
+    y_pos - new_size,
+    x_pos + new_size,
+    y_pos,
     x_pos,
-    mountain.y_pos
+    y_pos
   );
 
-  fill(47, 103, 125);
-  triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos + 100,
-    mountain.y_pos,
-    x_pos,
-    mountain.y_pos
-  );
+  // shadow
+  fill(126, 123, 166);
+  beginShape();
+  vertex(x_pos + new_size / 2, y_pos - new_size);
+  vertex(x_pos + new_size / 2, y_pos - (6 * new_size) / 8);
+
+  // for zig-zag effect on mountain
+  for (i = 6, i >= 0; i--; ) {
+    x_offset = i % 2 == 0 ? 9 : 7;
+    vertex(x_pos + (x_offset * new_size) / 16, y_pos - (i * new_size) / 8);
+  }
+
+  vertex(x_pos, y_pos);
+  endShape();
+
+  // top
+
+  fill(209, 234, 255);
+  beginShape();
+  vertex(x_pos + new_size / 2, y_pos - new_size);
+  vertex(x_pos + (6 * new_size) / 16, y_pos - (6 * new_size) / 8);
+  vertex(x_pos + (8 * new_size) / 16, y_pos - (6 * new_size) / 8);
+  endShape();
 
   fill(255, 255, 255);
-  triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos - 25,
-    mountain.y_pos - 169,
-    x_pos - 20,
-    mountain.y_pos - 150
-  );
-  triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos - 25,
-    mountain.y_pos - 169,
-    x_pos - 7,
-    mountain.y_pos - 155
-  );
-
-  fill(199, 228, 240);
-  triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos - 9,
-    mountain.y_pos - 175,
-    x_pos + 9,
-    mountain.y_pos - 147
-  );
-  triangle(
-    x_pos - 10,
-    mountain.y_pos - 199,
-    x_pos,
-    mountain.y_pos - 169,
-    x_pos + 17,
-    mountain.y_pos - 155
-  );
+  beginShape();
+  vertex(x_pos + new_size / 2, y_pos - new_size);
+  vertex(x_pos + (8 * new_size) / 16, y_pos - (6 * new_size) / 8);
+  vertex(x_pos + (10 * new_size) / 16, y_pos - (6 * new_size) / 8);
+  endShape();
 }
 
 function body() {
@@ -295,33 +349,33 @@ function foot(x, y, width, height) {
 }
 
 function leg(x1, y1, x2, y2) {
-  stroke(255, 255, 255);
+  stroke(130, 213, 255);
   strokeWeight(3.2);
   line(gameChar_x + x1, gameChar_y + y1, gameChar_x + x2, gameChar_y + y2);
 }
 
 function bentLeg(x1, y1, x2, y2, x3, y3, x4, y4) {
-  stroke(255, 255, 255);
+  stroke(130, 213, 255);
   strokeWeight(2.8);
   line(gameChar_x + x1, gameChar_y + y1, gameChar_x + x2, gameChar_y + y2);
   line(gameChar_x + x3, gameChar_y + y3, gameChar_x + x4, gameChar_y + y4);
 }
 
 function crossedArm(x1, y1, x2, y2, x3, y3, x4, y4) {
-  stroke(255, 255, 255);
+  stroke(130, 213, 255);
   strokeWeight(3);
   line(gameChar_x + x1, gameChar_y + y1, gameChar_x + x2, gameChar_y + y2);
   line(gameChar_x + x3, gameChar_y + y3, gameChar_x + x4, gameChar_y + y4);
 }
 
 function arm(x1, y1, x2, y2) {
-  stroke(255, 255, 255);
+  stroke(130, 213, 255);
   strokeWeight(3);
   line(gameChar_x + x1, gameChar_y + y1, gameChar_x + x2, gameChar_y + y2);
 }
 
 function hand(x, y) {
-  stroke(255, 255, 255);
+  stroke(130, 213, 255);
   strokeWeight(5);
   point(gameChar_x + x, gameChar_y + y);
 }
